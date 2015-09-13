e1e67ea66ff212bdcf37987e0e4c91867ba17299
Sarah R <phoesarah@hotmail.com>
2015-09-13 10:13:26 -0500

    Pull by DL

--- GlobalHackMVC/Controllers/HomeController.cs
+++ GlobalHackMVC/Controllers/HomeController.cs
@@ -18,11 +18,26 @@ namespace GlobalHackMVC.Controllers
         [HttpPost]
         public ActionResult Index(FormCollection collection)
         {
+            List<citation> model = new List<citation>();
             string fname = Request.Form["first_name"];
             string lname = Request.Form["last_name"];
             string dob = Request.Form["date_of_birth"];
             string dl = Request.Form["drivers_license_number"];
-            var model = db.citations.Where(x => x.first_name == fname).ToList();
+            var firstnames = db.citations.Where(x => x.first_name == fname).ToList();
+            var lastnames = db.citations.Where(x => x.last_name == lname).ToList();
+            //List<citation> dobs = new List<citation>();
+            //if ()
+            //    dobs = db.citations.Where(x => x.date_of_birth == Convert.ToDateTime(dob)).ToList();
+            var dls = db.citations.Where(x => x.drivers_license_number == dl).ToList();
+            
+            if(string.IsNullOrEmpty(dob))
+            {
+                foreach(var item in dls)
+                {
+                    model = db.citations.Where(x => x.drivers_license_number == dl).ToList();
+                }
+            }
+            
 
             return View("ListCitations", model);
         }
